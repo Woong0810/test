9ecf38754662dcb721aa34c9c6db828efe3881d0
Woong0810 <chan081010@naver.com>
2025-09-15 01:37:39 +0900

    알고리즘 과제

--- algo_practice.py
+++ algo_practice.py
@@ -1,7 +1,4 @@
-import sys, os, importlib.util, pathlib
-print("script dir =", pathlib.Path(__file__).parent)
-print("cwd       =", os.getcwd())
-print("in sys.path?", str(pathlib.Path(__file__).parent) in [str(pathlib.Path(p)) for p in sys.path if p])
+from data_unsorted_a_lot import numbers
+numbers = numbers[:1000]
 
-spec = importlib.util.find_spec("data_unsorted_a_lot")
-print("find_spec =", spec and spec.origin)
+print(numbers)
