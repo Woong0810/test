0feae33b02784b3f532444f67f0dfcd492c21474
Woong0810 <chan081010@naver.com>
2025-09-15 14:20:04 +0900

    9.15 알고리즘 수업

--- algo_practice.py
+++ algo_practice.py
@@ -1,4 +1,28 @@
-from data_unsorted_a_lot import numbers
-numbers = numbers[:1000]
+from data_unsorted import numbers
+# from data_unsorted_a_lot import numbers
 
-print(numbers)
+from random import randint, seed
+# from pyvisalgo import BubbleSortVisualizer as Visualizer
+from pyvisalgo import BubbleSortVisualizer as Visualizer
+# from pyvisalgo import Dummy as Visualizer
+
+def main():
+    print('before:', array)
+    count = len(array)
+    end = count - 1
+    if True:  # 들여쓰기를 위해 쓴다
+        for i in range(end):
+            vis.compare(i, i + 1)
+            if array[i] > array[i + 1]:
+                vis.compare(i, i + 1)
+                array[i], array[i + 1] = array[i + 1], array[i]
+    print('after :', array)
+
+
+''' Bubble 을 한 칸 진행해 본 결과, 다음과 같이 출력된다.
+before: [71, 30, 18, 51, 77, 37, 3, 93, 90, 48]
+after : [30, 18, 51, 71, 37, 3, 77, 90, 48, 93]
+'''
+
+if __name__ == '__main__':
+  seed('Hello') # 'Hello' 를 seed 로 고정하여 randint 가 항상 같은 결과가 나오게 한다
